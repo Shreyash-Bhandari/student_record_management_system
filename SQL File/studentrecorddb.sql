86ed17d8e7b5d11f04a0b0114553841d4a63f007
Shreyash-Bhandari <140000893+Shreyash-Bhandari@users.noreply.github.com>
2024-09-07 00:18:31 +0530

    Update studentrecorddb.sql

--- SQL File/studentrecorddb.sql
+++ SQL File/studentrecorddb.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2024 at 06:12 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
